79336fcfe7cb348aa0eaf94459cb41756ec7760a
MTLKS <43807318+MTLKS@users.noreply.github.com>
2024-12-15 15:41:07 +0800

    add openssh

--- profiles/permafrost/configuration.nix
+++ profiles/permafrost/configuration.nix
@@ -2,6 +2,11 @@
 
 {
     networking.hostName = "permafrost";
+    networking.firewall = {
+        enable = true;
+        allowedTCPPorts = [ 22 ];
+        allowedUDPPorts = [ 22 ];
+    };
 
     time.timeZone = "Asia/Kuala_Lumpur";
     i18n.defaultLocale = "en_US.UTF-8";
@@ -11,6 +16,8 @@
         extraGroups = [ "wheel" "networkmanager" "audio" ];
         home = "/home/mtlks";
     };
+
+    services.openssh.enable = true;
 
     nixpkgs.config.allowUnfree = true;
 }
