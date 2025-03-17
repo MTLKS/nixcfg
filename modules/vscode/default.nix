15c60a04985df6e9b1f12142a8b0e9df9bfa3076
MTLKS <43807318+MTLKS@users.noreply.github.com>
2025-03-18 04:55:59 +0800

    update vscode

--- modules/vscode/default.nix
+++ modules/vscode/default.nix
@@ -9,28 +9,30 @@ in {
     programs.vscode = {
       enable = true;
       package = pkgs.vscode-fhs;
-      extensions = with pkgs.vscode-extensions; [
-        bbenoist.nix
-        github.copilot
-        humao.rest-client
-        viktorqvarfordt.vscode-pitch-black-theme
-      ];
-      userSettings = {
-        "[nix]" = {
-          "editor.tabSize" = 2;
-        };
-        "debug.console.fontFamily" = "'CommitMono Nerd Font', monospace";
-        "editor.fontFamily" = "'CommitMono Nerd Font', monospace";
-        "editor.fontWeight" = "bold";
-        "editor.renderWhitespace" = "all";
-        "terminal.integrated.defaultProfile.linux" = "fish";
-        "terminal.integrated.fontFamily" = "'CommitMono Nerd Font', monospace";
-        "terminal.integrated.fontWeightBold" = "bold";
-        "window.titleBarStyle" = "custom";
-        "workbench.colorTheme" = "Pitch Black";
-        "workbench.colorCustomizations" = {
-          "editorWhitespace.foreground" = "#4B5263";
-          "sideBar.border" = "#222222";
+      profiles.default = {
+        extensions = with pkgs.vscode-extensions; [
+          bbenoist.nix
+          github.copilot
+          humao.rest-client
+          viktorqvarfordt.vscode-pitch-black-theme
+        ];
+        userSettings = {
+          "[nix]" = {
+            "editor.tabSize" = 2;
+          };
+          "debug.console.fontFamily" = "'CommitMono Nerd Font', monospace";
+          "editor.fontFamily" = "'CommitMono Nerd Font', monospace";
+          "editor.fontWeight" = "bold";
+          "editor.renderWhitespace" = "all";
+          "terminal.integrated.defaultProfile.linux" = "fish";
+          "terminal.integrated.fontFamily" = "'CommitMono Nerd Font', monospace";
+          "terminal.integrated.fontWeightBold" = "bold";
+          "window.titleBarStyle" = "custom";
+          "workbench.colorTheme" = "Pitch Black";
+          "workbench.colorCustomizations" = {
+            "editorWhitespace.foreground" = "#4B5263";
+            "sideBar.border" = "#222222";
+          };
         };
       };
     };
