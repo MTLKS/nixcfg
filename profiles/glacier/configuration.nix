48b2ead845c0ae93350fd7d0bafcd4db4e000fda
MTLKS <43807318+MTLKS@users.noreply.github.com>
2025-03-18 13:40:15 +0800

    add mini-greeter

--- profiles/glacier/configuration.nix
+++ profiles/glacier/configuration.nix
@@ -21,6 +21,35 @@
 
     services.xserver = {
       enable = true;
+      displayManager.lightdm = {
+        enable = true;
+        greeters.mini = {
+          enable = true;
+          user = "mtlks";
+          extraConfig = ''
+            [greeter]
+            show-password-label = false
+            invalid-password-text = hello?
+            password-alignment = center
+
+            [greeter-theme]
+            font = Commit Mono Nerd Font
+            font-size = 14
+            font-weight = bold
+            text-color = #CECDC3
+            error-color = #D14D41
+            background-image =
+            background-color = #282726
+            window-color = #100F0F
+            border-color = #E6E4D9
+            border-width = 2px
+            layout-space = 15
+            password-color = #CECDC3
+            password-background-color = #1C1B1A
+            password-border-width = 0px
+          '';
+        };
+      };
       windowManager.i3.enable = true;
       videoDrivers = ["nvidia"];
     };
