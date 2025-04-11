50b393896c3fb4e343d50b6a7ead754b9bd05217
MTLKS <43807318+MTLKS@users.noreply.github.com>
2025-04-12 02:31:53 +0800

    update xrandr command

--- profiles/glacier/configuration.nix
+++ profiles/glacier/configuration.nix
@@ -23,34 +23,39 @@
   services.xserver = {
     enable = true;
 
-    displayManager.lightdm = {
-    enable = true;
-    greeters.mini = {
-      enable = true;
-      user = "mtlks";
-      extraConfig = ''
-      [greeter]
-      show-password-label = false
-      invalid-password-text = hello?
-      password-alignment = center
-
-      [greeter-theme]
-      font = Commit Mono Nerd Font
-      font-size = 14
-      font-weight = bold
-      text-color = #CECDC3
-      error-color = #D14D41
-      background-image =
-      background-color = #282726
-      window-color = #100F0F
-      border-color = #E6E4D9
-      border-width = 2px
-      layout-space = 15
-      password-color = #CECDC3
-      password-background-color = #1C1B1A
-      password-border-width = 0px
+    displayManager = {
+      sessionCommands = ''
+        xrandr --output DP-1 --mode 1920x1080 --rate 60 --pos 0x0 --rotate left --scale 1.5x1.5 --output HDMI-0 --mode 3840x2160 --rate 120 --pos 1620x720 --primary --dpi 144 --scale 1x1 --output DP-3 --mode 1920x1080 --rate 60 --pos 5460x0 --rotate right --scale 1.5x1.5
       '';
-    };
+      lightdm = {
+        enable = true;
+        greeters.mini = {
+          enable = true;
+          user = "mtlks";
+          extraConfig = ''
+          [greeter]
+          show-password-label = false
+          invalid-password-text = hello?
+          password-alignment = center
+
+          [greeter-theme]
+          font = Commit Mono Nerd Font
+          font-size = 14
+          font-weight = bold
+          text-color = #CECDC3
+          error-color = #D14D41
+          background-image =
+          background-color = #282726
+          window-color = #100F0F
+          border-color = #E6E4D9
+          border-width = 2px
+          layout-space = 15
+          password-color = #CECDC3
+          password-background-color = #1C1B1A
+          password-border-width = 0px
+          '';
+        };
+      };
     };
     videoDrivers = ["nvidia"];
     windowManager.i3.enable = true;
