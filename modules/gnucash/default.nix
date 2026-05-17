1abf418dce1e1b02f490144ea5c2e2f832e3c940
MTLKS <43807318+MTLKS@users.noreply.github.com>
2026-05-17 13:03:16 +0800

    add gnucash desktop file

--- modules/gnucash/default.nix
+++ modules/gnucash/default.nix
@@ -9,5 +9,14 @@ in {
     home.packages = with pkgs; [
       gnucash
     ];
+    home.file.".local/share/applications/gnucash.desktop".text = ''
+      [Desktop Entry]
+      Name=GnuCash
+      GenericName=Finance Management
+      Exec=env WEBKIT_DISABLE_DMABUF_RENDERER=1 gnucash
+      Icon=gnucash-icon
+      Type=Application
+      Categories=Office;Finance;
+    '';
   };
 }
